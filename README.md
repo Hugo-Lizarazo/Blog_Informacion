c276d93e3dff36ad1a80425ea4ecb2d0318536ff
Hugo-Lizarazo <hlizarazoj@gmail.com>
2022-10-09 17:27:34 -0500

    Sincronizar con el directorio en mi mac

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Blog_Informacion
 Información y las Ciencias de la Información
+Blog sobre los concpetos de información y su relación con las ciencias de la información
---
+++ js/zoom.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+<script>
+$(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
+</script>
